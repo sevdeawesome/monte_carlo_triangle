1dc6e76e7be3c74408c7e00b96bc19739f1815ad
Severin Field <sevdeawesome@gmail.com>
2021-10-30 00:47:09 -0700

    added pseudocode

--- main.cpp
+++ main.cpp
@@ -4,8 +4,32 @@
 using namespace std;
 
 int main(){
-    
+    int side_length = 100;
+    int num_in, num_out = 0;
+    cout << num_out;
+// side_length = a
 
-    
+// int num_in, num_out
+
+//     for( n runs ){
+//         rand1 = a * rand()
+//         rand2 = a * rand()
+//         rand3 = a * rand()
+
+//         if(center within triangle){
+//           num_in++
+//         }
+//         else{
+//           num_out++
+//         }
+
+//     }
+
+
+// center_within_triangle(a1,a2,a3){
+  
+// }
+
+
     return 0;
 }
