46e858d85731eb2d91e5b66b98b4345423506b67
Severin Field <sevdeawesome@gmail.com>
2021-10-30 01:02:56 -0700

    sdasd

--- main.cpp
+++ main.cpp
@@ -4,9 +4,21 @@
 using namespace std;
 
 int main(){
+    int NUM_RUNS = 10000;
     int side_length = 100;
     int num_in, num_out = 0;
-    cout << num_out;
+
+    // double rand1 = (rand() % 101); //random number between 0 and 100
+
+
+    for(int i = 0; i < NUM_RUNS; i++){
+        double rand1 = side_length * (rand() % 1001); //random number between 0 and 1000 * side_length
+        double rand2 = side_length * (rand() % 1001);
+        double rand3 = side_length * (rand() % 1001);
+
+        cout << rand1 << " " << rand2 << " " << rand3;
+    }
+
 // side_length = a
 
 // int num_in, num_out
